8283e559c5d2754fdafb09609cf2f16adb77744d
Unknown <zoudingyun@foxmail.com>
2018-09-10 17:27:45 +0800

    zdy

---
+++ PortControllerServer/ThreadMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PortControllerServer
+{
+    class ThreadMessage
+    {
+        public TcpClient tc1;
+        public TcpClient tc2;
+        public String TOTAL_IP;
+        public String TOTAL_PORT;
+        public String type;
+    }
+}
